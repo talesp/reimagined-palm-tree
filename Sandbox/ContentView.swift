eb3b7a9ec9882da1a2644d0109fb52d83bfa685a
Tales Pinheiro de Andrade <tpid@novonordisk.com>
2023-06-02 12:46:30 +0200

    Initial Commit

---
+++ Sandbox/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Sandbox
+//
+//  Created by TPID (Tales Pinheiro de Andrade) on 02/06/2023.
+//
+
+// vary cool mistake :D
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by TPID (Tales Pinheiro de Andrade) on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
